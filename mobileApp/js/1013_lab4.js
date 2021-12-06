16e905a2c1b8265d3b84b86d9039af7c2d9d85a8
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-12-06 18:52:42 +0900

    .

--- mobileApp/js/1013_lab4.js
+++ mobileApp/js/1013_lab4.js
@@ -8,6 +8,8 @@ var P = [1234, 3347, 1111, 1212];
 export default function App() {
   var L = [];
 
+
+
   for (var i = 0; i < N.length; i++){
     var a = <Text style={{ fontSize: 20 }}>{N[i]} : {P[i]}</Text>
     L.push(a);
